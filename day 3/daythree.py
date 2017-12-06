2191762edc957c08dd0c0430c9e9abdc55cea2fe
Martin Karlsson <xenovoyance@gmail.com>
2017-12-06 13:02:58 +0100

    Messed up the spiral, revert.

--- day 3/daythree.py
+++ day 3/daythree.py
@@ -1,67 +1,64 @@
-# initialize the matrix
-matrix = [[0 for i in xrange(1000)] for i in xrange(1000)]
+# initialize the p1_matrix
+p1_matrix = [[0 for i in xrange(1000)] for i in xrange(1000)]
 
-matrix[0][0] = 1
-matrix[1][0] = 2
-matrix[1][1] = 3
+p1_matrix[0][0] = 1
+p1_matrix[1][0] = 2
+p1_matrix[1][1] = 3
 
 last_x = 1
 last_y = 1
 
 # define progression path logic selectors
-rep_counter = 2
-rep_counter_iterator = 2
-rep_switch = False
-direction = "left"
+p1_rep_counter = 2
+p1_rep_counter_iterator = 2
+p1_rep_switch = False
+p1_direction = "left"
 
 x = 4
 
-# build the spiral and break when we find our number
+# build the P1 spiral and break when we find our number
 while (x < 300000):
-	if (rep_counter_iterator > 0):
-		if (direction == "left"):
+	if (p1_rep_counter_iterator > 0):
+		if (p1_direction == "left"):
 			last_x = last_x -1
-			matrix[last_x][last_y] = x
-			if (x == 277678):
-				break
+			p1_matrix[last_x][last_y] = x
 			x = x + 1
-			rep_counter_iterator = rep_counter_iterator -1
-			if (rep_counter_iterator == 0):
-				direction = "down"
-		elif (direction == "down"):
+			p1_rep_counter_iterator = p1_rep_counter_iterator -1
+			if (p1_rep_counter_iterator == 0):
+				p1_direction = "down"
+		elif (p1_direction == "down"):
 			last_y = last_y -1
-			matrix[last_x][last_y] = x
-			if (x == 277678):
-				break
+			p1_matrix[last_x][last_y] = x
 			x = x + 1
-			rep_counter_iterator = rep_counter_iterator -1
-			if (rep_counter_iterator == 0):
-				direction = "right"
-		elif (direction == "right"):
+			p1_rep_counter_iterator = p1_rep_counter_iterator -1
+			if (p1_rep_counter_iterator == 0):
+				p1_direction = "right"
+		elif (p1_direction == "right"):
 			last_x = last_x +1
-			matrix[last_x][last_y] = x
-			if (x == 277678):
-				break
+			p1_matrix[last_x][last_y] = x
 			x = x + 1
-			rep_counter_iterator = rep_counter_iterator -1
-			if (rep_counter_iterator == 0):
-				direction = "up"
-		elif (direction == "up"):
-			last_y = last_y +1
-			matrix[last_x][last_y] = x
-			if (x == 277678):
-				break
+			p1_rep_counter_iterator = p1_rep_counter_iterator -1
+			if (p1_rep_counter_iterator == 0):
+				p1_direction = "up"
+		elif (p1_direction == "up"):
+			last_y = last_y + 1
+			p1_matrix[last_x][last_y] = x
 			x = x + 1
-			rep_counter_iterator = rep_counter_iterator -1
-			if (rep_counter_iterator == 0):
-				direction = "left"
+			p1_rep_counter_iterator = p1_rep_counter_iterator -1
+			if (p1_rep_counter_iterator == 0):
+				p1_direction = "left"
 	else:
-		if (rep_switch):
-			rep_counter = rep_counter +1
-			rep_switch = False
+		if (p1_rep_switch):
+			p1_rep_counter = p1_rep_counter +1
+			p1_rep_switch = False
 		else:
-			rep_switch = True
-		rep_counter_iterator = rep_counter
+			p1_rep_switch = True
+		p1_rep_counter_iterator = p1_rep_counter
+	if (x > 277678):
+		break
+
+if ((abs(last_x)+abs(last_y)) != 475):
+	print "!!! [ERROR] in P1 sprial! Should be 475 but was " + str(abs(last_x)+abs(last_y))
 
 # print the distance from number to center
-print abs(last_x)+abs(last_y)
+print "P1: " + str(abs(last_x)+abs(last_y))
