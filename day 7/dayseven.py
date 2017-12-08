5f78603307fd1b38f463d4ae5cdfff93cd29b796
Martin Karlsson <xenovoyance@gmail.com>
2017-12-08 14:40:13 +0100

    Part 1 done.

--- day 7/dayseven.py
+++ day 7/dayseven.py
@@ -2,6 +2,13 @@
 result = []
 itr = 0
 
+def find_parent_index(nodename):
+    "Finding the index in list of the parent for the child sent in"
+    for y in range(0,len(result)):
+        if result[y]['name'] == nodename:
+            return y
+    return -1
+
 with open('input_dayseven.txt') as f:
     # first, let's map names and weights into the dict result
     for line in f:
@@ -16,15 +23,13 @@ with open('input_dayseven.txt') as f:
         result.append(dict)
         itr = itr + 1
 
+with open('input_dayseven.txt') as f:
     # second time, let's map up parents
     for line in f:
         words = line.split()
-        print len(words)
-        """
         if len(words) > 2:
             for x in range(3,len(words)):
-                # find parent for the node we're at and update the dict
-                print words[x]
-                """
+                result[find_parent_index(words[x].strip(','))]['parent'] = words[0]
 
-#print result
+result.sort()
+print "Top node is: " + result[0]['name']
